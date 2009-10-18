144654f199dfeff1707940c85e8cc33d4ba6614a
deadlyomen17 <deadlyomen17@50981c16-bc11-11de-bb27-3175f2afd961>
2009-10-18 20:01:20 +0000

    working good examples of jme and jme physics NB: you have to set up your eclipse o netbeans ide to build jme2 if not follow this guides http://jmonkeyengine.com/wiki/doku.php?id=downloading_and_installing_jme_2

---
+++ src/customGame/Game1.java
@@ -0,0 +1,146 @@
+package customGame;
+
+import com.jme.app.BaseGame;
+import com.jme.bounding.BoundingBox;
+import com.jme.image.Texture;
+import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
+import com.jme.math.Vector3f;
+import com.jme.renderer.Camera;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Sphere;
+import com.jme.scene.state.TextureState;
+import com.jme.system.DisplaySystem;
+import com.jme.system.JmeException;
+import com.jme.util.TextureManager;
+import com.jme.util.Timer;
+
+/** Classe Game1:
+ * Primo game customizzato. Si estende BaseGame e non SimpleGame.
+ * Tutto quello che fa questa classe è visualizzare solo una sfera!
+ * @author slash17
+ */
+public class Game1 extends BaseGame {
+	// parametri
+	private int width, height, depth, freq;
+	private boolean fullscreen;
+	private Camera cam;
+	protected Timer timer;
+	// ts per caricare ed applicare textures
+	private TextureState ts;
+	// scene sarà il nostro "rootNode"
+	private Node scene;
+	
+	// metodi
+	public static void main(String[] args) {
+		Game1 application = new Game1();
+		application.setConfigShowMode(ConfigShowMode.AlwaysShow, Game1.class.getClassLoader().getResource( "images/openGame.jpg" ));
+		application.start();
+	}
+	
+	@Override
+	protected void cleanup() {
+		// prima di chiudere mi assicuro di cancellare tutte le textures caricate
+		ts.deleteAll();
+	}
+
+	@Override
+	protected void initGame() {
+		// inizializziamo il nostro rootNode
+		scene = new Node("Scene graph node");
+	 
+		// creiamo una sfera
+		Sphere s = new Sphere("Sphere", 30, 30, 25);
+		s.setLocalTranslation(new Vector3f(0, 0, -40));
+		s.setModelBound(new BoundingBox());
+		s.updateModelBound();
+		// le diamo un volto
+		ts = display.getRenderer().createTextureState();
+		ts.setEnabled(true);
+		ts.setTexture(TextureManager.loadTexture(
+				Game1.class.getClassLoader().getResource("images/rockWall2.jpg"),
+				Texture.MinificationFilter.BilinearNearestMipMap, 
+				Texture.MagnificationFilter.Bilinear));
+
+		s.setRenderState(ts);
+
+		scene.attachChild(s);
+
+		// aggiorniamo il rootNode visto che abbiamo aggiunto oggetti e textures
+		scene.updateGeometricState(0.0f, true);
+		scene.updateRenderState();
+	}
+
+	@Override
+	protected void initSystem() {
+		// impostiamo i parametri di visualizzazione
+		width = this.settings.getWidth();
+		height = this.settings.getHeight();
+		depth = this.settings.getDepth();
+		freq = this.settings.getFrequency();
+		fullscreen = this.settings.isFullscreen();
+ 
+		// impostiamo il nostro dysplaySystem, inizializziamo la finestra e creo la camera 
+		// (è in un try/catch per uscire in caso di problemi nella creazione della finestra)
+		try {
+			display = DisplaySystem.getDisplaySystem(this.getNewSettings().getRenderer());
+			display.createWindow(width, height, depth, freq, fullscreen);
+ 
+			cam = display.getRenderer().createCamera(width, height);
+		} catch (JmeException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+ 
+		// impostiamo lo sfondo nero classico
+		display.getRenderer().setBackgroundColor(ColorRGBA.black);
+ 
+		// inizializziamo la camera a 45° dalla scena, è solo un esempio!
+		cam.setFrustumPerspective(45.0f, (float)width / (float)height, 1, 1000);
+		Vector3f loc = new Vector3f(0.0f, 0.0f, 30.0f);
+		Vector3f left = new Vector3f(-1.0f, 0.0f, 0.0f);
+		Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
+		Vector3f dir = new Vector3f(0.0f, 0f, -1.0f);
+		// la piazzo ed oriento dove voglio
+		cam.setFrame(loc, left, up, dir);
+		
+		// abbiamo cambiato la camera quindi aggiorniamo
+		cam.update();
+ 
+		// usiamo il timer per calcolarci gli fps, ma può servire per molto altro
+        timer = Timer.getTimer();
+        
+        // impostiamo la camera al displaySystem
+		display.getRenderer().setCamera(cam);
+ 
+		// gestiamo ancora un solo evento, cioè la pressione del tasto "Esc" per chiudere il gioco
+		KeyBindingManager.getKeyBindingManager().set("exit", KeyInput.KEY_ESCAPE);
+	}
+
+	@Override
+	protected void reinit() {
+		// ricreo la mia finestra
+		display.recreateWindow(width, height, depth, freq, fullscreen);
+	}
+
+	@Override
+	protected void render(float interpolation) {
+		// cancello tutto
+		display.getRenderer().clearBuffers();
+		// ridisegno la scena
+		display.getRenderer().draw(scene);
+
+	}
+
+	@Override
+	protected void update( float interpolation ) {
+		// aggiorniamo il timer e il framerate
+		timer.update();
+		interpolation = timer.getTimePerFrame();
+		// usciamo se è stato dato il comando exit (tasto esc)
+		if (KeyBindingManager.getKeyBindingManager().isValidCommand("exit")) {
+			finished = true;
+		}
+	}
+}
---
+++ src/flagRush/CollisionDetection.java
@@ -0,0 +1,125 @@
+package flagRush;
+
+import java.util.logging.Logger;
+
+import jmetest.effects.RenParticleEditor;
+import com.jme.bounding.BoundingBox;
+import com.jme.image.Texture;
+import com.jme.math.FastMath;
+import com.jme.math.Vector3f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Controller;
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
+import com.jme.scene.state.BlendState;
+import com.jme.scene.state.RenderState;
+import com.jme.scene.state.TextureState;
+import com.jme.system.DisplaySystem;
+import com.jme.util.TextureManager;
+import com.jmex.effects.particles.ParticleFactory;
+import com.jmex.effects.particles.ParticleSystem;
+import com.jmex.effects.particles.SimpleParticleInfluenceFactory;
+
+public class CollisionDetection{
+ 
+    public static final int BounceOff = 0;
+    public static final int Stop = 1;
+ 
+	private Vehicle player;
+	private Node target;
+	private int NextMove;
+	private ParticleSystem particleGeom;
+	private float Zextent;
+ 
+	public CollisionDetection(Vehicle Player, Node Target,int NextMove) {
+        this.player = Player;
+        this.target = Target;
+        this.NextMove = NextMove;
+        this.Zextent =((BoundingBox)player.getWorldBound()).zExtent;
+        createParticles();
+    }
+	public CollisionDetection(Vehicle Player, Spatial Target,int NextMove) {
+        this.player = Player;
+        this.target = (Node)Target;
+        this.NextMove = NextMove;
+        this.Zextent =((BoundingBox)player.getWorldBound()).zExtent;
+        createParticles();
+    }
+ 
+    public void ProcessCollisions() {
+    	if (NextMove == BounceOff){//se la collisione è a rimbalzo
+               if (player.hasCollision(target, false)){
+            	//se la ruota anteriore collide con l'obiettivo(uno dei nodi dell'albero fence)
+        		if (player.frontwheel.hasCollision(target, false)){
+        			player.setVelocity(-Math.abs(player.getVelocity()));//inverto velocità
+        			if (player.getVelocity() > -0.5)//porto la velocità ad un minimo fissato
+        				player.setVelocity(-0.5f);
+        			//imposto le particelle che scaturiscono dall'impatto
+            		        particleGeom.setLocalTranslation(0F,player.frontwheel.getLocalTranslation().y*0.0025f, Zextent);
+        			particleGeom.forceRespawn();
+        		} else if(player.backwheel.hasCollision(target, false)){
+        			player.setVelocity(Math.abs(player.getVelocity()));
+        			if (player.getVelocity() < 0.5)
+        				player.setVelocity(0.5f);
+            		        particleGeom.setLocalTranslation(0F,player.backwheel.getLocalTranslation().y*0.0025f, -Zextent);
+        			particleGeom.forceRespawn();
+        		}
+        	}
+    	}else if (NextMove == Stop){
+               if (player.hasCollision(target, false)){
+        		if (player.frontwheel.hasCollision(target, false)){
+                                player.setMaxSpeed(0f);
+                                player.setMinSpeed(15f);
+        		} else if(player.backwheel.hasCollision(target, false)){
+                                player.setMaxSpeed(25f);
+                                player.setMinSpeed(0f);
+        		} else {
+                                player.setMaxSpeed(25f);
+                                player.setMinSpeed(15f);
+                        }
+        	}
+    	}
+    }
+ 
+ 
+    private void createParticles(){
+    	//imposto tipo di reazione alla collisione
+		particleGeom = ParticleFactory.buildParticles("collsion", 300);
+    	particleGeom.addInfluence(SimpleParticleInfluenceFactory
+    			.createBasicGravity(new Vector3f(0, -0.01f, 0), true));
+    	particleGeom.setEmissionDirection(new Vector3f(0.0f, 1.0f, 0.0f));
+    	particleGeom.setMaximumAngle(360f * FastMath.DEG_TO_RAD);
+    	particleGeom.setMinimumLifeTime(2000.0f);
+    	particleGeom.setMaximumLifeTime(4000.0f);
+    	particleGeom.setInitialVelocity(.004f);
+    	particleGeom.recreate(FastMath.nextRandomInt(6, 20));
+    	particleGeom.setStartSize(.05f);
+    	particleGeom.setEndSize(.02f);
+    	particleGeom.setStartColor(new ColorRGBA(1.0f, 0.796875f, 0.1992f, 1.0f));
+    	particleGeom.setEndColor(new ColorRGBA(1.0f, 1.0f, 0.5976f, 1.0f));
+    	//particleGeom.warmUp(120);
+    	particleGeom.getParticleController().setRepeatType(Controller.RT_CLAMP);
+ 
+    	BlendState as = (BlendState) particleGeom.getRenderState(RenderState.RS_BLEND);
+    	if (as == null) {
+        	as = DisplaySystem.getDisplaySystem().getRenderer()
+        		.createBlendState();
+        	as.setBlendEnabled(true);
+        	as.setSourceFunction(BlendState.SourceFunction.SourceAlpha);
+        	as.setTestEnabled(true);
+        	as.setTestFunction(BlendState.TestFunction.GreaterThan);
+        	particleGeom.setRenderState(as);
+        	particleGeom.updateRenderState();
+    	}
+    	as.setDestinationFunction(BlendState.DestinationFunction.One);
+    	TextureState ts = DisplaySystem.getDisplaySystem().getRenderer().createTextureState();
+    	ts.setTexture(TextureManager.loadTexture(RenParticleEditor.class
+            	.getClassLoader().getResource(
+                    	"jmetest/data/texture/flaresmall.jpg"),
+                    Texture.MinificationFilter.BilinearNearestMipMap, Texture.MagnificationFilter.Bilinear));
+    	particleGeom.setRenderState(ts);
+ 
+    	player.attachChild(particleGeom);
+    	particleGeom.updateRenderState();
+    }
+}
