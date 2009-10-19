1d086a145271208258ea1930906c9010a0782505
joseph@masterdrive.it <joseph@masterdrive.it@50981c16-bc11-11de-bb27-3175f2afd961>
2009-10-19 22:06:17 +0000

    UPDATE: Added a new package "joework.input.thirdperson" with class AssHoleThirdPersonHandler

---
+++ trunk/src/joework/input/thirdperson/AssHoleThirdPersonHandler.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package joework.input.thirdperson;
+
+import com.jme.input.InputHandler;
+
+/**
+ *
+ * @author joseph
+ */
+public class AssHoleThirdPersonHandler extends InputHandler {
+
+    // todo :D
+
+}
