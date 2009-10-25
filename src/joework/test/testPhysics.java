31b1d1f79d0f4de67b784dadbcb278b42d10c137
joseph@masterdrive.it <joseph@masterdrive.it@50981c16-bc11-11de-bb27-3175f2afd961>
2009-10-25 11:42:20 +0000

    UPDATED: removed the movements management from testPhysics.

--- src/joework/test/testPhysics.java
+++ src/joework/test/testPhysics.java
@@ -6,18 +6,10 @@
 package joework.test;
 
 import com.jme.input.FirstPersonHandler;
-import com.jme.input.InputHandler;
-import com.jme.input.KeyInput;
-import com.jme.input.action.InputAction;
-import com.jme.input.action.InputActionEvent;
-import com.jme.input.util.SyntheticButton;
 import com.jme.math.Vector3f;
 import com.jme.scene.shape.Box;
 import com.jmex.physics.DynamicPhysicsNode;
-import com.jmex.physics.PhysicsSpace;
-import com.jmex.physics.PhysicsUpdateCallback;
 import com.jmex.physics.StaticPhysicsNode;
-import com.jmex.physics.contact.ContactInfo;
 import joework.app.AssHoleBaseGame;
 
 /**
@@ -28,7 +20,6 @@ public class testPhysics extends AssHoleBaseGame {
 
     StaticPhysicsNode staticNode;
     DynamicPhysicsNode dynamicNode;
-    private boolean playerOnFloor = false;
 
     public static void main( String args[] ) {
         testPhysics m = new testPhysics();
@@ -83,72 +74,10 @@ public class testPhysics extends AssHoleBaseGame {
     @Override
     protected void setupInput() {
         input = new FirstPersonHandler(cam, 200, 1);
-
-        // i quattro movimenti Home - Fine - Canc - PgGi�
-        input.addAction( new MoveAction( new Vector3f( -2, 0, 0 ) ),
-                InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_H, InputHandler.AXIS_NONE, false );
-        input.addAction( new MoveAction( new Vector3f( 2, 0, 0 ) ),
-                InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_K, InputHandler.AXIS_NONE, false );
-        input.addAction( new MoveAction( new Vector3f( 0, 0, -2 ) ),
-                InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_U, InputHandler.AXIS_NONE, false );
-        input.addAction( new MoveAction( new Vector3f( 0, 0, 2 ) ),
-                InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_J, InputHandler.AXIS_NONE, false );
-
-        // per far saltare il nostro box applichiamo una forza verso l'alto (per il salto premere SPAZIO)
-        input.addAction( new InputAction() {
-            public void performAction( InputActionEvent evt ) {
-                if ( playerOnFloor && evt.getTriggerPressed() ) {
-                    dynamicNode.addForce( new Vector3f( 0, 500, 0 ) );
-                }
-            }
-        }, InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_SPACE, InputHandler.AXIS_NONE, false );
-
-        // evento di collisione
-        SyntheticButton playerCollisionEventHandler = dynamicNode.getCollisionEventHandler();
-        input.addAction( new InputAction() {
-            public void performAction( InputActionEvent evt ) {
-                ContactInfo contactInfo = (ContactInfo) evt.getTriggerData();
-                if ( contactInfo.getNode1() == staticNode || contactInfo.getNode2() == staticNode ) {
-                    playerOnFloor = true;
-                }
-            }
-        }, playerCollisionEventHandler, false );
-
-        // ad ogni step fisico reimpostiamo a false playerOnFloor
-        // in modo da non permettere salti quando si � gi� in aria
-        getPhysicsSpace().addToUpdateCallbacks( new PhysicsUpdateCallback() {
-            public void beforeStep( PhysicsSpace space, float time ) {
-                playerOnFloor = false;
-            }
-            public void afterStep( PhysicsSpace space, float time ) {
-            }
-        } );
     }
 
     @Override
     protected void simpleUpdate() {
         input.update(tpf);
-    }
-
-
-    /**
-     * Classe per gestire i movimenti
-     * non molto diversa da quella gi� vista
-     */
-    private class MoveAction extends InputAction {
-        private Vector3f direction;
-
-        public MoveAction( Vector3f direction ) {
-            this.direction = direction;
-        }
-        public void performAction( InputActionEvent evt ) {
-            if ( evt.getTriggerPressed() ) {
-                // quando viene premuto un testo, impostiamo il movimento nella direzione passata al costruttore
-                dynamicNode.addForce( direction.multLocal(2) );
-            } else {
-                // appena rilasciamo il tasto il movimento si azzera...
-                //dynamicNode.getMaterial().setSurfaceMotion( Vector3f.ZERO );
-            }
-        }
     }
 }
