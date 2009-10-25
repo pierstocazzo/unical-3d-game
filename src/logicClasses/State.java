f27cd23fa808213704c28e24f82c15a423c8728d
andreamartire@gmail.com <andreamartire@gmail.com@50981c16-bc11-11de-bb27-3175f2afd961>
2009-10-25 15:10:29 +0000

    [ADDED] Aggiunti stati rimanenti

--- src/logicClasses/State.java
+++ src/logicClasses/State.java
@@ -6,8 +6,16 @@ package logicClasses;
  * @author Andrea
  */
 public class State {
+	/** Stato di riposo */
 	final static int DEFAULT = 0;
-	final static int ALERT = 1;
-	final static int ATTACK = 2;
-	final static int DEATH = 3;
+	/** Stato di sentinella con percorso prefissato*/
+	final static int SENTINEL = 1;
+	/** Stato di guardia con posizione fissa */
+	final static int GUARD = 2;
+	/** Stato di allerta */
+	final static int ALERT = 3;
+	/** Stato di attacco */
+	final static int ATTACK = 4;
+	/** Stato di morte*/
+	final static int DEATH = 5;
 }
