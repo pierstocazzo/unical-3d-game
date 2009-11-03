1833bd189fd698b617e51890866b6d8a34a78529
deadlyomen17 <deadlyomen17@50981c16-bc11-11de-bb27-3175f2afd961>
2009-11-03 20:25:04 +0000

    UPDATE: added something

--- trunk/src/game/logic/LogicPlayer.java
+++ trunk/src/game/logic/LogicPlayer.java
@@ -9,7 +9,12 @@ import java.util.HashMap;
  */
 public class LogicPlayer extends LogicCharacter {
 
-	HashMap< String, LogicWeapon > equipment;
+	/** Player's equipment */
+	HashMap< Integer, LogicWeapon > equipment;
+	
+	int weaponCounter;
+	
+	LogicWeapon currentWeapon;
 	
 	/**
 	 * Constructor Object LogicPlayer
@@ -21,5 +26,34 @@ public class LogicPlayer extends LogicCharacter {
 	 */
 	public LogicPlayer( String id, int currentLife, int maxLife ) {
 		super( id, currentLife, maxLife );
+		
+		weaponCounter = 0;
+		
+		/** Initially the player has just the MP5 */
+		addWeapon( new LogicWeapon( "mp5", 100, WeaponType.MP5 ) );
+		changeWeapon( 1 );
+	}
+	
+	/** Function <code>addWeapon</code><br>
+	 * 
+	 * Add a new weapon to the player equipment.
+	 * @param weapon - (LogicWeapon) the weapon to add
+	 */
+	public void addWeapon( LogicWeapon weapon ) {
+		equipment.put( weaponCounter, weapon );
+		weaponCounter = weaponCounter + 1;
+	}
+	
+	/** Function <code>changeWeapon</code><br>
+	 * 
+	 * Switch to another weapon.
+	 * @param weaponKey - (int) the key of the weapon to use.
+	 */
+	public void changeWeapon( int weaponKey ) {
+		try {
+			currentWeapon = equipment.get( weaponKey );
+		} catch( Exception e ) {
+			// just don't change the current weapon
+		}
 	}
 }
--- trunk/src/game/logic/WeaponType.java
+++ trunk/src/game/logic/WeaponType.java
@@ -4,14 +4,22 @@ public enum WeaponType {
 	
 	MP5 ( 1, 1000000, 1000, false ),
 	
-	MACHINEGUN ( 10, 1000000, 1000, true ),
+	GATLING ( 10, 1000000, 1000, true ),
 	
 	BAZOOKA ( 30, 100000, 500, true );
 	
+	/** <code>WeaponType> field */
 	int damage, power, distance;
-	
 	boolean isHeavy;
 	
+	/**
+	 * <code>WeaponType</code> constructor<br>
+	 * Create a weapon type and set its attributes.
+	 * @param damage - (int) the damage the weapon cause to a character
+	 * @param power - (int) the shoot power of the weapon
+	 * @param distance - (int) the maximum distance a bullet of this ammo can reach
+	 * @param isHeavy - (bool) if true the character can't run when using this weapon
+	 */
 	WeaponType ( int damage, int power, int distance, boolean isHeavy ) {
 		this.damage = damage;
 		this.power = power;
