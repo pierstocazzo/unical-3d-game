662eb5e5a7195e9448c4e548d0816c7ca4523a34
deadlyomen17 <deadlyomen17@50981c16-bc11-11de-bb27-3175f2afd961>
2009-11-12 22:19:58 +0000

    UPDATE: added something

--- trunk/src/game/core/LogicAmmo.java
+++ trunk/src/game/core/LogicAmmo.java
@@ -2,12 +2,26 @@ package game.core;
 
 import com.jme.math.Vector3f;
 
-public class LogicAmmo extends LogicEnergyPack {
-	
+/** Class <code>LogicAmmo</code> <br>
+ * Represent pack of ammunitions the player can catch and use
+ */
+public class LogicAmmo {
+	/** type of the weapon whitch can use this ammo */
 	private EnumWeaponType type;
+	
+	/** quantity of ammunition */
 	private int quantity;
+	
+	/** the position of the ammo pack */
 	private Vector3f position;
 	
+	/** <code>LogicAmmo</code> constructor <br>
+	 * Create a new ammo pack
+	 * 
+	 * @param type - (EnumWeaponType) type of the weapon whitch can use this ammo
+	 * @param quantity - (int) quantity of ammunition the pack contains
+	 * @param position - (Vector3f) position of the ammo pack
+	 */
 	public LogicAmmo( EnumWeaponType type, int quantity, Vector3f position ){
 		this.type = type;
 		this.quantity = quantity;
