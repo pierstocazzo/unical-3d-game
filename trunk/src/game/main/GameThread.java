9c3b7074efb039edbd20f499df8c0f6ee6dff033
deadlyomen17 <deadlyomen17@50981c16-bc11-11de-bb27-3175f2afd961>
2009-12-25 11:09:15 +0000

    eliminato problema alla chiusura con audio...trovato problema in windows, il resume non porta il gioco in primo piano

--- trunk/src/game/main/GameThread.java
+++ trunk/src/game/main/GameThread.java
@@ -9,35 +9,29 @@ import com.jme.math.Vector3f;
 
 public class GameThread implements Runnable {
 
-//	public ThreadController threadController;
 	public LogicWorld logicGame;
 	public GraphicalWorld game;
 	public LoadingFrame loadingFrame;
 	
-	public GameThread(LoadingFrame loadingFrame ) {
+	public GameThread( LoadingFrame loadingFrame ) {
 		this.loadingFrame = loadingFrame;
-//		this.threadController = new ThreadController(loadingFrame,this);
 		logicGame = new LogicWorld();
     	logicGame.createPlayer( 100, new Vector3f( 700, 60, 700 ) );
     	logicGame.createEnemy( new Vector3f( 800, 50, 800 ), MovementType.REST );
 //    	logicGame.createEnemiesGroup( 3, new Vector3f( 50, 50, 50 ) );
 //    	logicGame.createEnemiesGroup( 4, new Vector3f( 220, 50, 220 ) );
-    	
-    	// TODO caricare pi� nemici
 //    	logicGame.createEnemiesGroup( 5, new Vector3f( 250, 10, 250 ) );
-//    	
 //    	logicGame.createEnemiesGroup( 20, new Vector3f( 1000, 10, 1000 ) );
-    	logicGame.createEnergyPackages( 20, 129*20, 129*20 );
+    	
+//    	logicGame.createEnergyPackages( 20, 129*20, 129*20 );
 	}
 	
 	public GameThread( LogicWorld logicGame, LoadingFrame loadingFrame ){
-//		this.threadController = new ThreadController(loadingFrame,this);
 		this.loadingFrame = loadingFrame;
 		this.logicGame = logicGame;
 	}
 
 	public void run() {
-//        game = new GraphicalWorld( logicGame, threadController );
 		game = new GraphicalWorld( logicGame, loadingFrame );
         game.start();
     }
