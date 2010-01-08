a66024598e57129a828f34046e2e04fdc8bea3b4
andreamartire <andreamartire@50981c16-bc11-11de-bb27-3175f2afd961>
2010-01-08 17:51:35 +0000

    eliminati import inutili

--- src/game/menu/OptionsMenu.java
+++ src/game/menu/OptionsMenu.java
@@ -1,26 +1,17 @@
 package game.menu;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
 import java.util.Vector;
 
-import javax.swing.Action;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
