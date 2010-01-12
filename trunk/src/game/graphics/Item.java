49f02382262ba1deeb375b403da8f46b85cd078c
deadlyomen17 <deadlyomen17@50981c16-bc11-11de-bb27-3175f2afd961>
2010-01-12 17:26:06 +0000

    caricamento scena da xml quasi completo...ma la chiusura non funziona bene

--- trunk/src/game/graphics/Item.java
+++ trunk/src/game/graphics/Item.java
@@ -1,32 +1,25 @@
-package game.common;
+package game.graphics;
 
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
-import com.jme.scene.Node;
 
 public class Item {
-	String fileName;
+
 	Vector3f position;
 	Quaternion rotation;
 	Vector3f scale;
-	Node model;
+	String meshId;
 	
 	public Item() {
-		this.fileName = "";
 		this.position = new Vector3f();
 		this.rotation = new Quaternion();
 		this.scale = new Vector3f();
 	}
 	
 	public Item( String fileName, Vector3f position, Quaternion rotation, Vector3f scale ) {
-		this.fileName = fileName;
 		this.position = position;
 		this.rotation = rotation;
 		this.scale = scale;
-	}
-	
-	public String getFileName() {
-		return fileName;
 	}
 
 	public Vector3f getPosition() {
@@ -39,13 +32,5 @@ public class Item {
 
 	public Vector3f getScale() {
 		return scale;
-	}
-	
-	public Node getModel() {
-		return model;
-	}
-	
-	public void setModel( Node model ) {
-		this.model = model;
 	}
 }
