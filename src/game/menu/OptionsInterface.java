e8bffc22198ecb45d95d70f14fac518d591a3a7a
andreamartire <andreamartire@50981c16-bc11-11de-bb27-3175f2afd961>
2010-01-12 09:28:19 +0000

    [UPDATE] Aggiunte dimenticanze

--- src/game/menu/OptionsInterface.java
+++ src/game/menu/OptionsInterface.java
@@ -4,37 +4,41 @@ public interface OptionsInterface {
 
 	public abstract String getDefaultResolution();
 	public abstract String getResolution();
-	public abstract void setResolution();
+	public abstract void setResolution( String value );
 	
 	public abstract boolean getDefaultIsFullscreen();
 	public abstract boolean isFullscreen();
-	public abstract void setFullscreen();
+	public abstract void setFullscreen( boolean value );
 	
 	public abstract String GetDefaultXmlPath();
 	public abstract String getXmlPath();
-	public abstract void setXmlPath();
+	public abstract void setXmlPath( String value );
 	
 	public abstract String GetDefaultForwardKey();
 	public abstract String getForwardKey();
-	public abstract void setForwardKey();
+	public abstract void setForwardKey( String value );
 	
 	public abstract String GetDefaultBackwardKey();
 	public abstract String getBackwardKey();
-	public abstract void setBackwardKey();
+	public abstract void setBackwardKey( String value );
 	
 	public abstract String GetDefaultStrafeRightKey();
 	public abstract String getStrafeRightKey();
-	public abstract void setStrafeRightKey();
+	public abstract void setStrafeRightKey( String value );
 	
 	public abstract String GetDefaultStrafeLeftKey();
 	public abstract String getStrafeLeftKey();
-	public abstract void setStrafeLeftKey();
+	public abstract void setStrafeLeftKey( String value );
 	
 	public abstract String GetDefaultRunKey();
 	public abstract String getRunKey();
-	public abstract void setRunKey();
+	public abstract void setRunKey( String value );
 	
 	public abstract String GetDefaultPauseKey();
 	public abstract String getPauseKey();
-	public abstract void setPauseKey();
+	public abstract void setPauseKey( String value );
+	
+	public abstract boolean getDefaultSoundEnabled();
+	public abstract boolean isSoundEnabled();
+	public abstract boolean setSoundEnabled( boolean value );
 }
